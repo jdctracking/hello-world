b0099174dd11ba09bad4e63a56e5dcf221cab9ff
David Panesso <jdcastan89@gmail.com>
2021-05-02 10:20:35 -0400

    added remove todo function

--- index.js
+++ index.js
@@ -29,9 +29,15 @@ function App(){
         setTodos(newTodos);
         setValue('');
     }
+    const removeTodo = e =>{
+        const index = Number(e.target.id);
+        let temp = [...todos];
+        temp.splice(index,1);
+        setTodos(temp);
+    }
     return(<>
         {todos.map((todo,i) => 
-            <div className="todo" key={i}>{todo.text}</div>)}
+            <div className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>)}
         <form onSubmit={handleSubmit}>
             <input type="text" className="input" value={value} placeholder="Add Todo ..." onChange={e => setValue(e.target.value)}/>
         </form>
